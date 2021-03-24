993a0ce1a58441e98ddb30415a3118b642170179
danielaucarpro <76978089+danielaucarpro@users.noreply.github.com>
2021-03-24 17:44:53 -0300

    second push
    add some imgs references, tried some javascript code, change the style

--- script.js
+++ script.js
@@ -1,59 +1,70 @@
-const state = "";
+context = c.getContext('2d');
+const myCanvas = document.getElementById('myCanvas');
+const interval = 24;
+playerSize = interval
+
+//number variables
 const playerLife = 3;
 const enemyLife = 1;
+const score = 0;
 
-while (state === title) {
-    let startBtn = document.getElementById('start');
+//Creating image class for my assets
+//player
+const player = new Image();
+player.src = './img/player.png'
+//player position
+playerX = 150;
+playerY = 225;
+playerSize;
 
-    //show title screen
+//enemy
+const enemy = new Image();
+//bullet
+const bullet = new Image();
+const positionToShoot = '';
+const shootVel = 10;
 
-    //play music
 
-    //start game
-    startBtn.onclick = function () { iniciateGame() };
-    function iniciateGame() {
-        state = game;
-    }
+
+setInterval(() => {
+    //score
+    context.fillStyle = 'white';
+    context.fillText(score++);
+
+    //draw enemies
+    //fill a rectangle with imgs
+    context.fillRect()
+
+    //draw player
+    context.drawImage(player, playerX, playerY, playerSize * (103 / 63), playerSize);
+
+    //player input and player movement
+    //right arrow
+    context.addEventListener('keydown', function (event) {
+        if (event.keyCode === 39) {
+            playerX += 1;
+        }
+        else if (playerX === 300) {
+            playerX += 0;
+        }
+    });
+    //left arrow
+    context.addEventListener('keydown', function (event) {
+        if (event.keyCode === 37) {
+            playerX -= 1;
+        }
+        else if (playerX === 0) {
+            playerX -= 0;
+        }
+    });
+    //space
+    context.addEventListener('keydown', function (event) {
+        if (event.keyCode === 32) {
+            shoot();
+        }
+    });
+}, interval)
+
+function shoot(){
+    //spawn shoot img in front of player
 }
-
-while (state === game) {
-    //CHANGE SCREEN
-    //clear screen
-    //display new one
-
-    //ENEMY
-    //spawn emenies (waves)
-    //death statement
-    //load sounds
-
-    //PLAYER OBJ
-    //spawn player
-    //get player shooting
-    //load sounds
-    //get player movement
-    //get input 
-
-    //GENERAL
-    //show lives
-    //show score
-
-    //if live = 0 change the state of the game to game over.
-    if (life <= 0) {
-        state = gameOver;
-    }
-}
-
-while (state === gameOver) {
-    //CHANGE SCREEN
-    //clear screen
-    //display new one
-
-    //register score
-
-    //set reset button
-}
-
-//
-//Vector 2d Obj
-//
-
